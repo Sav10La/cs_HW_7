9c6df6dfc20dc54f38bb3216604b0ef54e3a9a1e
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-19 11:51:08 +0500

    add Task047 RandomRoundDouble

---
+++ Task047/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+void Zadacha47()
+{
+    int rows = 3;
+    int columns = 4;
+    double[,] array = new double[rows, columns]; // 4 x 5
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine();
+    DecimalRound(array);
+}
+
+void FillArray(double[,] array)
+{
+    Random random = new Random();
+    int start = 1;
+    int end = 10;
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++) // array.Length or array.GetLength(0)
+    {
+        for (int j = 0; j < columns; j++) // array.Length or array.GetLength(1)
+        {
+            array[i, j] = random.NextDouble() * (end - start) + start;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++) // array.Length or array.GetLength(0)
+    {
+        for (int j = 0; j < columns; j++) // array.Length or array.GetLength(1)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void DecimalRound(double[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < rows; i++) // array.Length or array.GetLength(0)
+    {
+        for (int j = 0; j < columns; j++) // array.Length or array.GetLength(1)
+        {
+            Console.Write(Math.Round(array[i, j], 1, MidpointRounding.ToEven) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Zadacha47();
